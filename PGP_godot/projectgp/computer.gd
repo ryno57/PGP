1231a31a06c47e98e44fe4aa70a607514c7cbf76
Lothevy <tristanou.girardin@gmail.com>
2025-08-29 15:01:56 +0200

    Initial commit

---
+++ PGP_godot/projectgp/computer.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+func on_interact():
+	print("Interaction ordi")
+	# Plus tard : afficher une UI, charger des données, etc.
---
+++ PGP_godot/projectgp/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var speed := 5.0
+@export var mouse_sensitivity := 0.3
+
+@onready var camera := $Camera3D
+
+var rotation_y := 0.0
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_y -= event.relative.x * mouse_sensitivity
+		rotation_degrees.y = rotation_y
+		camera.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -80, 80)
+
+func _physics_process(delta):
+	var input_dir = Vector3.ZERO
+	input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_dir.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	input_dir = input_dir.rotated(Vector3.UP, deg_to_rad(rotation_y)).normalized()
+
+	velocity.x = input_dir.x * speed
+	velocity.z = input_dir.z * speed
+	move_and_slide()
+
+func _process(delta):
+	if Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding():
+		var target = $RayCast3D.get_collider()
+		if target.has_method("on_interact"):
+			target.call("on_interact")
